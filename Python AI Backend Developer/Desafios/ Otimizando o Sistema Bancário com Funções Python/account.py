001ae89d0ea03d0cd44e652570b703413e2a682d
EOS <elnataoliveira@gmail.com>
2024-04-29 21:55:33 -0300

    Update and rename conta.py to account.py

---
+++ Python AI Backend Developer/Desafios/ Otimizando o Sistema Bancário com Funções Python/account.py
@@ -0,0 +1,15 @@
+class Account:
+
+    LIMITE_DIARIO = 0
+    LIMITE_SAQUE = 500
+
+    def __init__(self, agency, client, nro):
+        self.agency = agency
+        self.nro = nro
+        self.client = client
+        self.saldo = 0
+
+
+
+
+
